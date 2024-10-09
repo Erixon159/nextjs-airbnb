29e70d67033f815c966412700bf72ac163a394b5
Erixon159 <erixon159@gmail.com>
2024-10-09 22:13:30 +0200

    Init app

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+
+export const metadata: Metadata = {
+  title: 'Next.js AirBnB',
+  description: 'Feel at home, away from home.',
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Navbar />
+        <main className="container py-10">{children}</main>
+      </body>
+    </html>
+  )
+}
